cc28c3b0cfd36dc1cdadfd6c662f5e882697c9c9
Taiiayo <taiiakinyo@gmail.com>
2018-09-20 18:20:01 +0300

    first commit. A simple RSS feed. Names aren't too good

---
+++ RSS demo/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace RSS_demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.addLinkButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // addLinkButton
+            // 
+            this.addLinkButton.Location = new System.Drawing.Point(42, 56);
+            this.addLinkButton.Name = "addLinkButton";
+            this.addLinkButton.Size = new System.Drawing.Size(123, 23);
+            this.addLinkButton.TabIndex = 1;
+            this.addLinkButton.Text = "Add";
+            this.addLinkButton.UseVisualStyleBackColor = true;
+            this.addLinkButton.Click += new System.EventHandler(this.addLinkButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.addLinkButton);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 450);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control Box";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(200, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(600, 450);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button addLinkButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+    }
+}
+
---
+++ RSS demo/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace RSS_demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addLinkButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                XmlReader fd_readxml = XmlReader.Create(textBox1.Text);
+                SyndicationFeed fd_feed = SyndicationFeed.Load(fd_readxml);
+
+                TabPage fd_tab = new TabPage(fd_feed.Title.Text);
+
+                tabControl1.TabPages.Add(fd_tab);
+
+                ListBox fd_list = new ListBox();
+                fd_tab.Controls.Add(fd_list);
+                fd_list.Dock = DockStyle.Fill;
+                fd_list.HorizontalScrollbar = true;
+
+                foreach (SyndicationItem fd_item in fd_feed.Items)
+                {
+                    string summary = fd_item.Summary.Text;
+                    bool isRunning = true;
+                    int numberOfLetters = 250;
+
+                    string fix_sum = "";
+
+
+                    foreach (char characterData in summary)
+                    {
+                        if (numberOfLetters >= 0 && characterData != '.' && isRunning)
+                        {
+                            numberOfLetters--;
+                            fix_sum = fix_sum + characterData;
+                        }
+                        else if (characterData == '.' && isRunning && (numberOfLetters <= 100 & numberOfLetters >= 0))
+                        {
+                            fix_sum = fix_sum + characterData;
+                            isRunning = false;
+                        }
+                        else if (isRunning && numberOfLetters >= 0)
+                        {
+                            fix_sum = fix_sum + characterData;
+                        }
+                    }
+
+                    fd_list.Items.Add(fd_item.Title.Text);
+                    fd_list.Items.Add(fix_sum);
+                    fd_list.Items.Add("~ ~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+    }
+}
